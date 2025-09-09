6e0c8fbacfd27ff7cac2395b8d1cc70902aeb112
nehorosiyparen <sir.kuzin09@yandex.ru>
2025-09-10 02:04:16 +0400

    update: ScrollToTop component

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { appRoutes } from './config/routes.config'
 import RouteWrapper from './RouteWrapper'
+import ScrollToTop from '../components/ScrollToTop'
 
 export const router = createBrowserRouter(
     appRoutes.map(route => ({
         path: route.path,
-        element: <RouteWrapper route={route}><route.element /></RouteWrapper>
+        element: 
+            <>
+                <ScrollToTop />
+                <RouteWrapper route={route}><route.element /></RouteWrapper>
+            </>
     }))
 )
